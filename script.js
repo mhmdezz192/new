64323dd535e62939783f1d638104bc4e8251e272
dell <71730632@students.liu.edu.lb>
2021-05-21 12:43:48 +0300

    updated

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark','aoun']
+  const labels = ['aoun','Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
